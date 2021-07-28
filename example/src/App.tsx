fd0d1ff9ce1198e1fc65de0584b3654183c12b2e
djgu <djgu@tmindtech.com>
2021-07-28 14:06:49 +0800

    init

---
+++ example/src/App.tsx
@@ -0,0 +1,31 @@
+/**
+ * title
+ */
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, SafeAreaView } from 'react-native';
+
+interface IProps {
+}
+
+interface IState {
+}
+
+export default class App extends Component<IProps, IState> {
+
+  constructor(props: IProps) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>App</Text>
+      </SafeAreaView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+});
